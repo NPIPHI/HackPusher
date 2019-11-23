d8d8375a98d72f405ddacdecbc73594865a956b1
Aniket Gargya <gargya.aniket@gmail.com>
2019-11-23 05:21:44 -0600

    Finally fixed the grid

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ function addGame(game, otherUser) {
     games.push(activeGame);
     document.getElementById("game-container").innerHTML = "";
     document.getElementById("game-container").appendChild(activeGame.element);
-    // document.getElementById("chatLog").style.height = 'calc(100vh - 487px - 50px)';
 }
 
 function sendGameMessage(message) {
